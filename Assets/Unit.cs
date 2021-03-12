e2e0708100fa1a1a7ee2463a0c951445537bdc8e
xogud02 <xogud02@gmail.com>
2021-03-12 23:54:12 +0900

    animator test

--- Assets/Unit.cs
+++ Assets/Unit.cs
@@ -4,7 +4,23 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    private Animator animator;
     public void Start() {
-        Debug.Log("hello world");
+        animator = GetComponent<Animator>();
+    }
+
+    public void Update() {
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            animator.SetTrigger("Up");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            animator.SetTrigger("Down");
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            animator.SetTrigger("Left");
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            animator.SetTrigger("Right");
+        }
     }
 }
