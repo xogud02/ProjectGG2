251839f381b2f6c80d7700fd6685869a0a4a60ea
xogud02 <xogud02@gmail.com>
2021-03-15 22:34:18 +0900

    floor enums define

---
+++ Assets/Floor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor {
+
+    public enum FloorShapeType {
+        TopLeft, Top, TopRight, /*          */VirticalTop,/*                    */ Single,
+        Left, Center, Right, /*             */VirticalCenter, HorizontalLeft, HorizontalCenter, HorizontalRight,
+        BottomLeft, Bottom, BottomRight, /* */VirticalBottom
+    }
+
+    public enum FloorBrightness {
+        Brighter,
+        Bright,
+        Dark,
+        Darker
+    }
+
+    public enum FloorMaterialType {
+        Brick, Grass, Bush,
+        Dirt, Wood, Lock,
+        Soil
+    }
+
+}
