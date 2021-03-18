031e3a6c01c603cfecd5e3faeb5a3fad533dc7cd
xogud02 <xogud02@gmail.com>
2021-03-18 23:27:18 +0900

    mod logic

--- Assets/Floor.cs
+++ Assets/Floor.cs
@@ -27,16 +27,20 @@ public class Floor {
     public static int GetFlooNumber(FloorShapeType floorShapeType, FloorBrightness floorBrightness, FloorMaterialType floorMaterialType) {
         var start = 32;
         var shapeCount = (int)FloorShapeType.VirticalBottom + 1;
-        var skipShapeCount = Mathf.Clamp((int)floorMaterialType / 3 - 1, 0, 2) * ((int)FloorBrightness.Darker + 1) * shapeCount;
-        skipShapeCount += Mathf.Clamp((int)floorBrightness - 1, 0, 3) * 3 * shapeCount;
-        var skipLine = 0;
+        var skipShapeCount = Mathf.Max((int)floorMaterialType / 3 - 1, 0) * ((int)FloorBrightness.Darker + 1) * shapeCount;
+        skipShapeCount += Mathf.Max((int)floorBrightness - 1, 0) * 3 * shapeCount;
+        var skipLines = 0;
+        var currentLine = (int)FloorShapeType.Single + 1;
         if (floorShapeType > FloorShapeType.HorizontalRight) {
-            skipLine = (int)FloorShapeType.HorizontalRight;
+            skipLines = (int)FloorShapeType.HorizontalRight + 1;
+            currentLine = FloorShapeType.VirticalBottom - FloorShapeType.HorizontalRight;
         } else if (floorShapeType > FloorShapeType.Single) {
-            skipLine = (int)FloorShapeType.Single;
+            skipLines = (int)FloorShapeType.Single + 1;
+            currentLine = FloorShapeType.HorizontalRight - FloorShapeType.Single;
         }
 
-        return start + skipShapeCount;
+
+        return start + skipShapeCount + skipLines * 3 + currentLine * (Mathf.Max((int)floorMaterialType % 3 - 1, 0));
     }
 
 }
