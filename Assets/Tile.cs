a0000c952c2ff96a3cb6a4e6c5f83309a6b0feac
xogud02 <xogud02@gmail.com>
2021-03-29 23:39:44 +0900

    tile test

--- Assets/Tile.cs
+++ Assets/Tile.cs
@@ -5,15 +5,30 @@ using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class Tile : MonoBehaviour {
+    private float size = 0f;
     void Start() {
-        var temp = new GameObject("foo");
-        var sr = temp.AddComponent<SpriteRenderer>();
-        var path = Floor.GetFloorPath(Floor.FloorShapeType.Center, Floor.FloorBrightness.Bright, Floor.FloorMaterialType.Grass);
-        Addressables.LoadAssetAsync<Sprite>(path).Completed += handle => {
-            if(handle.Status == AsyncOperationStatus.Succeeded) {
-                sr.sprite = handle.Result;
-            }
-        };
+        Foo(Floor.FloorShapeType.TopLeft, -1, -1);
+        Foo(Floor.FloorShapeType.Top, -1, 0);
+        Foo(Floor.FloorShapeType.TopRight, -1, 1);
+        Foo(Floor.FloorShapeType.Left, 0, -1);
+        Foo(Floor.FloorShapeType.Center, 0, 0);
+        Foo(Floor.FloorShapeType.Right, 0, 1);
+        Foo(Floor.FloorShapeType.BottomLeft, 1, -1);
+        Foo(Floor.FloorShapeType.Bottom, 1, 0);
+        Foo(Floor.FloorShapeType.BottomRight, 1, 1);
+
+        void Foo(Floor.FloorShapeType shapeType, int row, int col) {
+            var temp = new GameObject("foo");
+            var sr = temp.AddComponent<SpriteRenderer>();
+            var path = Floor.GetFloorPath(shapeType, Floor.FloorBrightness.Bright, Floor.FloorMaterialType.Grass);
+            Addressables.LoadAssetAsync<Sprite>(path).Completed += handle => {
+                if (handle.Status == AsyncOperationStatus.Succeeded) {
+                    sr.sprite = handle.Result;
+                    size = sr.size.x;
+                    temp.transform.position = new Vector2(col, -row) * size;
+                }
+            };
+        }
     }
 
     void Update() {
