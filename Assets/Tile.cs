3b7a4758c0703b82dc3dbd8c14d5857af0b390ac
xogud02 <xogud02@gmail.com>
2021-04-03 23:45:03 +0900

    no rowcol to integer xy

--- Assets/Tile.cs
+++ Assets/Tile.cs
@@ -7,17 +7,17 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 public class Tile : MonoBehaviour {
     private float size = 0f;
     void Start() {
-        Foo(Floor.FloorShapeType.TopLeft, -1, -1);
-        Foo(Floor.FloorShapeType.Top, -1, 0);
-        Foo(Floor.FloorShapeType.TopRight, -1, 1);
-        Foo(Floor.FloorShapeType.Left, 0, -1);
+        Foo(Floor.FloorShapeType.TopLeft, -1, 1);
+        Foo(Floor.FloorShapeType.Top, 0, 1);
+        Foo(Floor.FloorShapeType.TopRight, 1, 1);
+        Foo(Floor.FloorShapeType.Left, -1, 0);
         Foo(Floor.FloorShapeType.Center, 0, 0);
-        Foo(Floor.FloorShapeType.Right, 0, 1);
-        Foo(Floor.FloorShapeType.BottomLeft, 1, -1);
-        Foo(Floor.FloorShapeType.Bottom, 1, 0);
-        Foo(Floor.FloorShapeType.BottomRight, 1, 1);
+        Foo(Floor.FloorShapeType.Right, 1, 0);
+        Foo(Floor.FloorShapeType.BottomLeft, -1, -1);
+        Foo(Floor.FloorShapeType.Bottom, 0, -1);
+        Foo(Floor.FloorShapeType.BottomRight, 1, -1);
 
-        void Foo(Floor.FloorShapeType shapeType, int row, int col) {
+        void Foo(Floor.FloorShapeType shapeType, int x, int y) {
             var temp = new GameObject("foo");
             var sr = temp.AddComponent<SpriteRenderer>();
             var path = Floor.GetFloorPath(shapeType, Floor.FloorBrightness.Bright, Floor.FloorMaterialType.Grass);
@@ -25,7 +25,7 @@ public class Tile : MonoBehaviour {
                 if (handle.Status == AsyncOperationStatus.Succeeded) {
                     sr.sprite = handle.Result;
                     size = sr.size.x;
-                    temp.transform.position = new Vector2(col, -row) * size;
+                    temp.transform.position = new Vector2(x, y) * size;
                 }
             };
         }
