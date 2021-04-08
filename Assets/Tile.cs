bccaeadc92a8ff204fe907e3f82d0f0dd7dfd91e
xogud02 <xogud02@gmail.com>
2021-04-08 22:28:43 +0900

    add logic

--- Assets/Tile.cs
+++ Assets/Tile.cs
@@ -5,6 +5,13 @@ using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class Tile : MonoBehaviour {
+    public enum AdjType {
+        None = 0,
+        Up = 1 << 0,
+        Down = 1 << 1,
+        Left = 1 << 2,
+        Right = 1 << 3
+    }
     private float size = 0f;
     void Start() {
         GrassField(10, 10);
@@ -12,23 +19,37 @@ public class Tile : MonoBehaviour {
 
     private GameObject GrassField(int width, int height, float ratio = 0.3f) {
         var grass = new HashSet<Vector2Int>();
-        for(int x = 0; x < width; ++x) {
-            for(int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            for (int y = 0; y < height; ++y) {
                 if (Random.Range(0, 1f) > ratio) {
                     grass.Add(new Vector2Int(x, y));
                 }
             }
         }
+
         var ret = new GameObject("grassField");
         var dx = -width / 2f;
         var dy = -height / 2f;
-        for(int x = 0; x < width; ++x) {
-            for(int y = 0; y < height; ++y) {
+
+        var dic = new Dictionary<Vector2Int, AdjType> {
+            {Vector2Int.up, AdjType.Up },
+            {Vector2Int.down, AdjType.Down},
+            {Vector2Int.left, AdjType.Left},
+            {Vector2Int.right, AdjType.Right}
+        };
+        for (int x = 0; x < width; ++x) {
+            for (int y = 0; y < height; ++y) {
                 var newPos = new Vector2(x + dx, y + dy);
                 var tile = new GameObject("");
                 tile.transform.parent = ret.transform;
                 var sr = tile.AddComponent<SpriteRenderer>();
-                
+                AdjType adj = AdjType.None;
+                foreach(var kvp in dic) {
+                    if (grass.Contains(kvp.Key)) {
+                        adj |= kvp.Value;
+                    }
+                }
+
                 if (grass.Contains(new Vector2Int(x, y)) == false) {
                     var path = Floor.GetFloorPath(Floor.FloorShapeType.Center, Floor.FloorBrightness.Bright, Floor.FloorMaterialType.Dirt);
                     Addressables.LoadAssetAsync<Sprite>(path).Completed += handle => {
