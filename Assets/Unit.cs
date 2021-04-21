312a59688e37daadb0a4f675dcaedf3fd4c70a80
xogud02 <xogud02@gmail.com>
2021-04-21 09:24:23 +0900

    move linear

--- Assets/Unit.cs
+++ Assets/Unit.cs
@@ -11,20 +11,29 @@ public enum Direction {
     Right
 }
 
-public static class Move {
+public static class MoveExtension {
     public static Action<Vector2> OnMove;
     public static void Foo(this Vector2 position) => OnMove?.Invoke(position);
     public static void Foo(this Vector3 position) => OnMove?.Invoke(position);
     public static Vector3 STW(this Vector3 position) => Camera.main.ScreenToWorldPoint(position);
     public static Vector3 STW(this Vector2 position) => Camera.main.ScreenToWorldPoint(position);
+    public static Vector2 Convert(this Vector3 v) => v;
 }
 
 public class Unit : MonoBehaviour {
 
+    private float speed = 5;
+
     private Animator animator;
     public void Start() {
         animator = GetComponent<Animator>();
-        Move.OnMove = v => DOTween.To(() => (Vector2)transform.position, vec => transform.position = vec, v, 10);
+        MoveExtension.OnMove = Move;
+    }
+
+    public void Move(Vector2 v) {
+        var dist = Vector2.Distance(transform.position, v);
+        var time = dist / speed;
+        DOTween.To(() => (Vector2)transform.position, vec => transform.position = vec, v, time).SetEase(Ease.Linear);
     }
 
     public void Update() {
