1f7e66008f38948ff69fb7437b478504f8d49927
xogud02 <xogud02@gmail.com>
2021-04-22 09:45:38 +0900

    drection logic

--- Assets/Unit.cs
+++ Assets/Unit.cs
@@ -18,11 +18,15 @@ public static class MoveExtension {
     public static Vector3 STW(this Vector3 position) => Camera.main.ScreenToWorldPoint(position);
     public static Vector3 STW(this Vector2 position) => Camera.main.ScreenToWorldPoint(position);
     public static Vector2 Convert(this Vector3 v) => v;
+    public static Vector3 Convert(this Vector2 v) => v;
 }
 
 public class Unit : MonoBehaviour {
-
     private float speed = 5;
+    private readonly int Right = Animator.StringToHash("Right");
+    private readonly int Left = Animator.StringToHash("Left");
+    private readonly int Up = Animator.StringToHash("Up");
+    private readonly int Down = Animator.StringToHash("Down");
 
     private Animator animator;
     public void Start() {
@@ -31,7 +35,21 @@ public class Unit : MonoBehaviour {
     }
 
     public void Move(Vector2 v) {
-        var dist = Vector2.Distance(transform.position, v);
+        var direction = v - transform.position.Convert();
+        var angle = Vector2.SignedAngle(Vector2.right, direction);
+        var absAngle = Mathf.Abs(angle);
+        if (absAngle < 45f) {
+            animator.SetTrigger(Right);
+        } else if (absAngle > 135f) {
+            animator.SetTrigger(Left);
+        } else if (angle > 0) {
+            animator.SetTrigger(Up);
+        } else {
+            animator.SetTrigger(Down);
+        }
+
+
+        var dist = direction.magnitude;
         var time = dist / speed;
         DOTween.To(() => (Vector2)transform.position, vec => transform.position = vec, v, time).SetEase(Ease.Linear);
     }
