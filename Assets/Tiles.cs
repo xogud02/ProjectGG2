6af864ab578bad87ee979967fa406f8a2309b8f7
xogud02 <xogud02@gmail.com>
2021-04-28 21:35:39 +0900

    code cleanup

--- Assets/Tiles.cs
+++ Assets/Tiles.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.AsyncOperations;
 using Random = UnityEngine.Random;
 
 public class Tiles : MonoBehaviour {
