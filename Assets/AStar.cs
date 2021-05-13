86ee31a23839d0301698a7ae9cd5d7dc7121eb2c
xogud02 <xogud02@gmail.com>
2021-05-13 23:35:16 +0900

    add function body

--- Assets/AStar.cs
+++ Assets/AStar.cs
@@ -1,3 +1,8 @@
-﻿public static class AStar {
+﻿using System.Collections.Generic;
+using UnityEngine;
 
+public static class AStar {
+    public static Queue<Vector2Int> Find() {
+        return null;
+    }
 }
