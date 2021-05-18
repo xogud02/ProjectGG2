0ae102e722c6a8d4ca92ac0935b4cd0fca370644
xogud02 <xogud02@gmail.com>
2021-05-18 09:37:48 +0900

    private ctor

---
+++ Assets/GridField.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridField {
+    private GridField() { }
+    public static bool IsMovable(Vector2Int _) => false;
+}
