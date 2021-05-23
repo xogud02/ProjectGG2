7331b8cdecf2b46ba6ff742a6d969f8c124e5deb
xogud02 <xogud02@gmail.com>
2021-05-23 11:24:37 +0900

    add param

--- Assets/AStar.cs
+++ Assets/AStar.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 
 public static class AStar {
-    public static Queue<Vector2Int> Find() {
-        return null;
+    public static Queue<Vector2Int> Find(Vector2Int from, Vector2Int to) {
+        return new Queue<Vector2Int>();
     }
 }
