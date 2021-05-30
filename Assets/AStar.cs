dbc893d474949807bca0400e5e5691ee0408942d
xogud02 <xogud02@gmail.com>
2021-05-30 09:35:47 +0900

    define astar node type

--- Assets/AStar.cs
+++ Assets/AStar.cs
@@ -2,7 +2,17 @@
 using UnityEngine;
 
 public static class AStar {
+    private struct Node {
+        public readonly float distance;
+        public float sum;
+        public Node(float distance, float sum = 0) {
+            this.distance = distance;
+            this.sum = sum;
+        }
+    }
+
     public static Queue<Vector2Int> Find(Vector2Int from, Vector2Int to) {
+        var start = new Node((to - from).magnitude);
         return new Queue<Vector2Int>();
     }
 }
