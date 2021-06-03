35b1dcf14d47eb2f46eba41876f3db7a2733fa57
xogud02 <xogud02@gmail.com>
2021-06-03 22:20:27 +0900

    astar node modify

--- Assets/AStar.cs
+++ Assets/AStar.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 public static class AStar {
     private struct Node {
         public readonly float distance;
-        public float sum;
-        public Node(float distance, float sum = 0) {
+        public readonly float moved;
+        public readonly float sum;
+        public Node(float distance, float moved = 0) {
             this.distance = distance;
-            this.sum = sum;
+            this.moved = moved;
+            sum = distance + moved;
         }
     }
 
 
     public static Queue<Vector2Int> Find(Vector2Int from, Vector2Int to) {
         var start = new Node((to - from).magnitude);
-        var open = new SortedSet<Node>(Comparer<Node>.Create((node1, node2) => (int)(node1.sum - node2.sum))) { start };
+        var open = new SortedSet<Node>(Comparer<Node>.Create((node1, node2) => (int)(node1.sum- node2.sum))) { start };
         var closed = new HashSet<Node>();
         var ret = new Queue<Vector2Int>();
         while (open.Count > 0) {
