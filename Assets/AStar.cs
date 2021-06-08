063c137c88542e74b564e5ac31214c247a948400
xogud02 <xogud02@gmail.com>
2021-06-08 22:56:17 +0900

    more lines

--- Assets/AStar.cs
+++ Assets/AStar.cs
@@ -19,13 +19,19 @@ public static class AStar {
     public static Queue<Vector2Int> Find(Vector2Int from, Vector2Int to) {
         var start = new Node(from, to);
         var open = new SortedSet<Node>(Comparer<Node>.Create((node1, node2) => (int)(node1.sum - node2.sum))) { start };
-        var closed = new HashSet<Node>();
+        var closed = new HashSet<Vector2Int>();
         var ret = new Queue<Vector2Int>();
         var dx = new[] { -1, 0, 1, -1, 0, 1, -1, 0, 1 };
         var dy = new[] { -1, -1, -1, 0, 0, 0, 1, 1, 1 };
         while (open.Count > 0) {
             var current = open.Min;
             open.Remove(current);
+            for(var i = 0; i < dx.Length; ++i) {
+                var next = new Vector2Int(dx[i], dy[i]) + current.position;
+                if (closed.Contains(next)) {
+                    continue;
+                }
+            }
         }
         return ret;
     }
