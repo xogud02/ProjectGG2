d2b4c70df59acc754822c1dfcc81315487fc158c
xogud02 <xogud02@gmail.com>
2021-06-16 21:54:17 +0900

    more field

--- Assets/GridField.cs
+++ Assets/GridField.cs
@@ -18,10 +18,16 @@ public class GridField {
     public static Action<Vector2Int> OnClick;
     public static bool IsInRange(Vector2Int pos) => 0 <= pos.x && pos.x < Width && 0 <= pos.y && pos.y < Height;
     public static bool IsMovable(Vector2Int _) => IsInRange(_) && GetTileType(_) != TileType.Block;
-    public static int Width => 100;
-    public static int Height => 100;
+    public static int Width => Instance.width;
+    public static int Height => Instance.height;
     public static TileType GetTileType(Vector2Int _) => TileType.Block;
     public static void AddObject(GridObject gridObject) {
         Instance.objects.Add(gridObject);
+    }
+
+    private int width, height;
+    public void Init(int width, int height) {
+        this.width = width;
+        this.height = height;
     }
 }
