3c02c3bb86ab490a3c46bd807b129a94bfb8ea02
xogud02 <xogud02@gmail.com>
2021-08-23 22:01:44 +0900

    few line

--- Assets/CameraControl.cs
+++ Assets/CameraControl.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraControl : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class CameraControl : MonoBehaviour {
+    [SerializeField] private Camera _camera;
+    private void Awake() {
+        if(_camera != null) {
+            _camera = Camera.main;
+        }
+    }
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
 }
