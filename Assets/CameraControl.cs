260733b1e4dd27f4af83260f535d8dc621159eb5
xogud02 <xogud02@gmail.com>
2021-08-28 11:40:58 +0900

    modify cam

--- Assets/CameraControl.cs
+++ Assets/CameraControl.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour {
     [SerializeField] private Camera _camera;
+    [SerializeField] private Unit _focused;
     private void Awake() {
         if(_camera != null) {
             _camera = Camera.main;
