24465540fb515adc5e9c721aaae318c57c110d00
xogud02 <xogud02@gmail.com>
2021-10-08 21:59:13 +0900

    load anim

--- Assets/GUIBar.cs
+++ Assets/GUIBar.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
-public class GUIBar : MonoBehaviour
-{
+public class GUIBar : MonoBehaviour {
     public RuntimeAnimatorController[] controllers;
 
+    protected void Awake() {
+        controllers = new RuntimeAnimatorController[5];
+        for (int i = 0; i < 4; ++i) {
+            var capture = i;
+            Addressables.LoadAssetAsync<RuntimeAnimatorController>($"GUI_{41 + i}").Completed += _ => controllers[capture] = _.Result;
+        }
+    }
 }
