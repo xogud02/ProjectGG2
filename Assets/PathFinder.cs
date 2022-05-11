70176c9700f3480265cd6187f53847cb4118cd89
xogud02 <xogud02@naver.com>
2022-05-11 22:36:46 +0900

    introduce class

---
+++ Assets/PathFinder.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder
+{
+    public bool TryGetNextTile(out Vector2Int ret)
+    {
+        ret = Vector2Int.zero;
+        return false;
+    }
+}
