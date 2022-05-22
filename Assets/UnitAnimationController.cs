7e83cdcdecb2d4b655a28ebe46ed1c331d8d6751
xogud02 <xogud02@naver.com>
2022-05-22 13:57:19 +0900

    logic

--- Assets/UnitAnimationController.cs
+++ Assets/UnitAnimationController.cs
@@ -2,22 +2,36 @@ using UnityEngine;
 
 public class UnitAnimationController
 {
+    private enum MoveDirection
+    {
+        None,
+        Up,
+        UpRight,
+        Right,
+        DownRight,
+        Down,
+        DownLeft,
+        Left,
+        UpLeft,
+    }
+
     private readonly Animator _animator;
     private readonly int Right = Animator.StringToHash("Right");
     private readonly int Left = Animator.StringToHash("Left");
     private readonly int Up = Animator.StringToHash("Up");
     private readonly int Down = Animator.StringToHash("Down");
-    private int currentDirection;
+    private int _lastDirectionHash;
+    private MoveDirection _lastMoveDirection;
 
     public UnitAnimationController(Animator animator) => _animator = animator;
 
     public void RefreshDirection(Vector2 direction)
     {
         var preferDirection = GetPreferDirection(Vector2.SignedAngle(Vector2.right, direction));
-        if (preferDirection != currentDirection)
+        if (preferDirection != _lastDirectionHash)
         {
             _animator.SetTrigger(preferDirection);
-            currentDirection = preferDirection;
+            _lastDirectionHash = preferDirection;
         }
     }
 
@@ -40,5 +54,17 @@ public class UnitAnimationController
         {
             return Down;
         }
+    }
+
+    private MoveDirection ToMoveDirection(Vector2 direction)
+    {
+        var signedAngle = Vector2.SignedAngle(Vector2.right, direction);
+        var absAngle = Mathf.Abs(signedAngle);
+        return MoveDirection.None;
+    }
+
+    private int GetDirectionHash(MoveDirection direction)
+    {
+        return Up;
     }
 }
